ee297cf5e8572c62eb145a875f524b187a1fc289
Alireza H Abadi <61397285+Misfit-Ha@users.noreply.github.com>
2023-05-10 21:24:26 +0330

    .

--- embeds.js
+++ embeds.js
@@ -1,16 +1,16 @@
 
 function buildNewMissionEmbed (description, imageUrl) {
-  var newDescription = description.replace(/━/g, '').replace(/┏╋◥◣◆◢◤╋┓/g, '').replace(/┗╋◥◣◆◢◤╋┛/g, '').replace(/•/g, '');
-  var newDescription = newDescription.split('💥برای')[0];
+  //var newDescription = description.replace(/┏╋◥◣◆◢◤╋┓/g, '').replace(/┗╋◥◣◆◢◤╋┛/g, '').replace(/┏◥◣◆◢◤┓/g, '').replace(/┗◥◣◆◢◤┛/g, '');
+  let newDescription = description.split('━')[0];
   
   const embed = {
     color: 0x00FF1A,
-    title: 'پست جدید',
+    title: '',
     url: 'https://t.me/PersianArma3',
     author: {
-      name: 'Forwarded From Telegram',
-      icon_url: 'https://toppng.com/uploads/preview/telegram-icon-telegram-logo-11563072765e0pl0xsrfe.png',
-      url: 'https://t.me/PersianArma3',
+      name: '',
+      icon_url: '',
+      url: '',
     },
     description: newDescription || '',
     image: {
@@ -18,7 +18,7 @@ function buildNewMissionEmbed (description, imageUrl) {
   },
       timestamp: new Date(),
         footer: {
-          text: 'این پیغام به صورت خودکار از کانال تلگرام گرفته شده ',
+          text: 'پیام خودکار',
             icon_url: 'https://toppng.com/uploads/preview/telegram-icon-telegram-logo-11563072765e0pl0xsrfe.png',
         },
 };
@@ -26,19 +26,3 @@ function buildNewMissionEmbed (description, imageUrl) {
 return embed;  
 }
 
-      
-function buildNewScreenshotEmbed (imageUrl) {
-  const embed = {
-    color: 0xFF001A,
-    image: {
-      url: imageUrl,
-      timestamp: new Date(),
-    },
-    footer: {
-      text: 'این پیغام به صورت خودکار از کانال تلگرام گرفته شده ',
-      icon_url: 'https://toppng.com/uploads/preview/telegram-icon-telegram-logo-11563072765e0pl0xsrfe.png',
-    },
-  };
-  
-  return embed;  
-}
